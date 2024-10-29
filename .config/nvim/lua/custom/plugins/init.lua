1706f5c87f620d6e120c94ce2ec42fa6a1fbd8ad
Yotam Gurfinkel <yotam706@gmail.com>
2024-10-29 21:59:01 +0200

    Add neovim config

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,35 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+--
+return {
+  {
+    "chrisgrieser/nvim-spider",
+    opts = {
+      consistentOperatorPending = true
+    },
+    keys = {
+      {
+        "cw",
+        "c<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n" }
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      },
+    },
+  },
+}
